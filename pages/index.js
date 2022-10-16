a9b83927bd8873bf6f831035706e3862caaabf67
colorlessenergy <brianmunozdev@gmail.com>
2022-10-15 20:35:27 -0700

    add container class

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Nav />
+            <div className="container">
+                <Nav />
+            </div>
         </div>
     );
 }
