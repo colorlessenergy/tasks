c435c133afcd6b226e0cf44690db1d18882208cb
colorlessenergy <brianmunozdev@gmail.com>
2022-10-17 12:08:57 -0700

    add theme selector functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        if (!localStorage.getItem('theme')) {
+            if (
+                window.matchMedia &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches
+            ) {
+                localStorage.setItem('theme', 'dark');
+            } else {
+                localStorage.setItem('theme', 'light');
+            }
+        }
+
+        if (localStorage.getItem('theme') === 'dark') {
+            document.body.classList.add('dark');
+        }
+    }, []);
     return <Component {...pageProps} />;
 }
 
